c8bcd287f5c3e405f625cfd7cc50461ffabb4aff
Ysabel Katchy <ykatchy@gmail.com>
2023-02-10 14:52:02 +0100

    UI UPdates

--- Assets/ScriptTemplates/DemoScripts/Scripts/CustomScript.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-// Date created: " + DateTime.Now.ToString("MM/dd/yyyy")
-public class CustomScript :MonoBehaviour
-{
-    
-}
