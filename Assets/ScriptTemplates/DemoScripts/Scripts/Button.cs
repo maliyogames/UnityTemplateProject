93ab2bb2d1b9256f23c064f3559d374b8ada36fa
Ysabel Katchy <ykatchy@gmail.com>
2023-02-10 14:59:02 +0100

    UI Update

---
+++ Assets/ScriptTemplates/DemoScripts/Scripts/Button.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+// Date created: " + DateTime.Now.ToString("MM/dd/yyyy")
+public class Button : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log("Check");
+        }
+}
---
+++ Assets/ScriptTemplates/DemoScripts/Scripts/Maliyo_Class.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Date created: " + DateTime.Now.ToString("MM/dd/yyyy")
+public class Maliyo_Class :MonoBehaviour
+{
+    
+}
