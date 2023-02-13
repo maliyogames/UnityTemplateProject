c36167ba241eaf6158f3b1d12e33244e1eebe542
Ysabel Katchy <ykatchy@gmail.com>
2023-02-13 16:01:40 +0100

    Update

--- Assets/Scripts/DemoScripts/Maliyo_Class.cs
+++ Assets/Scripts/DemoScripts/Maliyo_Class.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// Date updated: " + DateTime.Now.ToString("MM/dd/yyyy")
+// Date updated: 2023-02-13
 public class Maliyo_Class :MonoBehaviour
 {
     void Start()
     {
         //Add your code
+        
     }
     void Update()
     {
