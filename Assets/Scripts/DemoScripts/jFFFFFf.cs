c94ef96ed2503810ccd684822c82be7b0c2ad8ec
Ysabel Katchy <ykatchy@gmail.com>
2023-02-13 15:45:05 +0100

    Update

---
+++ Assets/Scripts/DemoScripts/jFFFFFf.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Date updated: " + DateTime.Now.ToString("MM/dd/yyyy")
+public class jFFFFFf :MonoBehaviour
+{
+    void Start()
+    {
+        //Add your code
+    }
+    void Update()
+    {
+        //Add your code
+    }
+}
