4f57ec0adfa35ae26119d57c0e898419dc98e3e0
Ysabel Katchy <ykatchy@gmail.com>
2023-02-13 15:22:45 +0100

    Update

--- Assets/Bash/main.bash
+++ Assets/Bash/main.bash
@@ -7,7 +7,7 @@ find "$folder_name" -type f -name "*.cs" | while read file_name; do
   # Check if the file contains the date comment
   if grep -q "Date updated: " "$file_name"; then
     # Replace the "Date created: " placeholder with the current date
-    sed "s/Date created:.*/Date created: $current_date/" "$file_name" > "$file_name".tmp
+    sed "s/Date updated:.*/Date created: $current_date/" "$file_name" > "$file_name".tmp
   else
     # Add the date comment if it does not exist
     echo "// Date updated: $current_date" | cat - "$file_name" > "$file_name".tmp
