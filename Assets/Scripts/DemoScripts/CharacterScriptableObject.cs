1f16e8afd8616ebb77c418139e238af0b449bee8
Ysabel Katchy <ykatchy@gmail.com>
2023-03-01 00:19:18 +0100

    BUG FIXES :-(

---
+++ Assets/Scripts/DemoScripts/CharacterScriptableObject.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+// Date updated: " + DateTime.Now.ToString("MM/dd/yyyy")
+[CreateAssetMenu(fileName = "CharacterScriptableObject", menuName = "CharacterScriptableObject")]
+public class CharacterScriptableObject : ScriptableObject
+{
+    //Add your code
+}
